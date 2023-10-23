be073a0a219174020f5a02265806d141c2980265
Hamza <89248055+hamza-hwshiar@users.noreply.github.com>
2023-10-23 23:47:55 +0300

    new files

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t hamzahwshiar/demo-app:jam-2.1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push hamzahwshiar/demo-app:jam-2.1'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
